f2379e3af1801f9be1c7ccc82e204002fcf318f0
Hashem Hashem <hashemmm96@gmail.com>
2017-03-11 21:34:23 +0100

    Converted to using express. There is a problem where the body of a POST request can't be sent back to the requester.

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
 'use strict'
 
+// Essential requirements.
 var express = require('express');
-// var readline = require('readline');
-// var google = require('googleapis');
-// var util = require('util');
 var GoogleSpreadsheet = require('google-spreadsheet');
 var async = require('async');
+var bodyParser = require('body-parser');
+// Here you sepcify which google sheet to use. The ID of the sheet is
+// the long part of the URL to the sheet.
 var doc = new GoogleSpreadsheet('1E-Z2AOufVk533XaEwyRGx0IVrXqrYuwGI0WtBN550h8');
 var sheet;
 var app = express();
 
+// Code to load a google sheet. Needs a google service account.
 async.series([
 	function setAuth(step) {
-		var creds = require('./dbus_secret.json');
+		// json file from google, you get it from google developer console. 
+		var creds = require('./dbus_secret.json'); 
 		doc.useServiceAccountAuth(creds, step);
 	},
 	function getInfoAndWorksheets(step) {
 		doc.getInfo(function(err, info) {
-			console.log('Loaded doc: '+info.title+' by '+info.author.email);
 			sheet = info.worksheets[0];
+			// Debug messages.
+			console.log('Loaded doc: '+info.title+' by '+info.author.email);
 			console.log('sheet 1: '+sheet.title+' '+sheet.rowCount+'x'+sheet.colCount);
 			step();
 		});
@@ -28,52 +32,40 @@ async.series([
 function processForm(req, res) {
 	/* TODO Code for parsing form. Insert values of submitted fields
 	 * into google sheet. */
-				 
-	var bokning = req.body.bokning;
-	var name = req.body.name;
-	var mail = req.body.mail;
-	var fromDate = req.body.fromDate;
-	var fromTime = req.body.fromTime;
-	var fromCounter = req.body.fromCounter;
-	var toDate = req.body.toDate;
-	var toTime = req.body.toTime;
-	var toCounter = req.body.toCounter;
-	var message = req.body.message;
-	var faktura = req.body.faktura;
-	var innan = req.body.innan;
-	var efter = req.body.efter;
+		
+	// Variables for all the elements in the form.
+	// var bokning = req.body.bokning;
+	// var name = req.body.name;
+	// var mail = req.body.mail;
+	// var fromDate = req.body.fromDate;
+	// var fromTime = req.body.fromTime;
+	// var fromCounter = req.body.fromCounter;
+	// var toDate = req.body.toDate;
+	// var toTime = req.body.toTime;
+	// var toCounter = req.body.toCounter;
+	// var message = req.body.message;
+	// var faktura = req.body.faktura;
+	// var innan = req.body.innan;
+	// var efter = req.body.efter;
 
 	console.log(req.body);
 
-	// Old code used when express wasnt active
-	
-	// sheet.addRow(row, function(err, res) {
-	// 	if(err) throw err;
-	// 	console.log("wrote values");
-	// });
-	
-	// For debugging purposes-------------------------------------------------
-	// form.parse(req, function (err, fields, files) {
-	// 	console.log(row);
-	// 	sheet.addRow(row, function(err, res) {
-	// 		if(err) throw err;
-	// 		console.log("wrote");
-	// 	});
-
-    //     //Store the data from the fields in your data store.
-    //     //The data store could be a file or database or any other store based
-    //     //on your application.
-    //     res.writeHead(200, {
-    //         'content-type': 'text/plain'
-    //     });
-    //     res.write('received the data:\n\n');
-    //     res.end(util.inspect({
-    //         fields: fields,
-    //         files: files
-    //     }));
-    // });
-	//------------------------------------------------------------------------
 }
+
+var jsonParser = bodyParser.json();
+
+// Startpage
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/form.html');
+});
+
+// Action taken on form submission
+app.post('/', jsonParser, function (req, res) {
+	//processForm(req, res);
+	//res.sendFile(__dirname + '/confirmation.html')
+	console.log(req.body);
+	res.json(req.body);
+});
 
 app.listen(3000);
 
