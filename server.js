f45c8203a2855a6e95ea1a98639c8d56173e457d
Hashem Hashem <hashemmm96@gmail.com>
2017-03-12 23:58:08 +0100

    Added calculation of costs and distance counting.

--- server.js
+++ server.js
@@ -28,19 +28,74 @@ async.series([
 	}
 ]);
 
+function calcDays(startDate, endDate, startTime, endTime) {
+	var oneDay = 24*60*60*1000; // ms
+	var start = new Date(startDate);
+	var end = new Date(endDate);
+	var days = Math.round(Math.abs((start.getTime() - end.getTime())/(oneDay)));
+	startTime = startTime.split(":");
+	endTime = endTime.split(":");
+	start = new Date(0, 0, 0, startTime[0], startTime[1], 0);
+	end = new Date(0, 0, 0, endTime[0], endTime[1], 0);
+	var minutes = Math.floor((end.getTime() - start.getTime()) / 1000 / 60);
+	if(minutes < 0) {
+		days--;
+	}
+	return days;
+}
+
+function calcCost(req, days, distance) {
+	var kmCost1;
+	var kmCost2;
+	var extra;
+	var dayCost;
+	switch(req.body.bokning) {
+	case "utomstående":
+		kmCost1 = 10;
+		kmCost2 = 5;
+		dayCost = 150;
+		extra = 50;
+		break;
+ 	default:
+		kmCost1 = 5;
+		kmCost2 = 2.5;
+		dayCost = 150;
+		extra = 0;
+		break;
+	}
+	if(distance < 100) {
+		return days*dayCost + distance*kmCost1 + extra;
+	}
+	else {
+		return days*dayCost + distance*kmCost1 + (distance - 100)*kmCost2 + extra;
+	}
+}
+
 function processForm(req, res) {
-	/* TODO Code for parsing form. Insert values of submitted fields
-	 * into google sheet. */
+	var distance = req.body.toCounter - req.body.fromCounter;
+	var days = calcDays(req.body.fromDate, req.body.toDate,
+						req.body.fromTime, req.body.toTime);
+	var cost = calcCost(req, days, distance);
 
 	// The values to be written to the sheet.
 	var row = {
-		reqbody : req.body,
-		lol : "hel"
+		bokning : req.body.bokning,
+		name : req.body.name,
+		mail : req.body.mail,
+		distance : distance,
+		from : req.body.fromDate + " " + req.body.fromTime,
+		to : req.body.toDate + " " + req.body.toTime,
+		message : req.body.message,
+		fakturatext : req.body.fakturatext,
+		innan : req.body.innan,
+		efter : req.body.efter,
+		duration : days + " " + dagar,
+		cost : cost
 	}
 
 	// Add form values and additional fields to sheet.
 	sheet.addRow(row, function(err){
-		if (err) throw err;
+		if(err) throw err;
 		console.log("Data added successfully.");
 	});
 }
@@ -57,6 +112,7 @@ app.get('/', function (req, res) {
 // Action taken on form submission
 app.post('/', urlParser, function (req, res) {
 	processForm(req, res);
+	// Load the confirmation page.
 	//res.sendFile(__dirname + '/confirmation.html')
 	console.log(req.body);
 	res.send(req.body);
